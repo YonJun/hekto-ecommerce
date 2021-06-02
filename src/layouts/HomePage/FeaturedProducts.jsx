d81fa2f7826def20cad00fc81ddc3117aa167943
YonJun <jhon_08_2007@hotmail.com>
2021-06-01 20:02:46 -0500

    FeaturedProducts fix min height card

--- src/layouts/HomePage/FeaturedProducts.jsx
+++ src/layouts/HomePage/FeaturedProducts.jsx
@@ -110,8 +110,11 @@ const LineClampStyle = css`
 `;
 const Product = ({ id, img, title, code, price }) => {
   return (
-    <div className="group" tw="bg-white text-navy-blue shadow-md w-full">
-      <div tw="relative bg-gray-50 py-5">
+    <div
+      className="group"
+      tw="bg-white text-navy-blue shadow-md w-full hover:bg-my-blue"
+      style={{ minHeight: 380 }}>
+      <div tw="relative bg-gray-50 py-5 ">
         <div tw="bg-red-500 absolute flex space-x-4 top-0 left-0 invisible p-3 group-hover:visible">
           <span>1</span>
           <span>2</span>
@@ -133,7 +136,7 @@ const Product = ({ id, img, title, code, price }) => {
           </button>
         </div>
       </div>
-      <div tw="flex flex-col space-y-2 text-center py-3 px-2 group-hover:(bg-my-blue text-white)">
+      <div tw="flex flex-col space-y-2 text-center py-3 px-2 group-hover:(text-white)">
         <h5
           css={[
             tw`font-semibold text-lg text-my-pink group-hover:text-white`,
@@ -162,7 +165,11 @@ const FreaturedProducts = () => {
                 id={idx}
                 code="Y523201"
                 price="42.00"
-                title="Cantilever chair Cantilever chairCantilever chair"
+                title={
+                  idx % 2 === 0
+                    ? "Cantilever chair Cantilever chair Cantilever chair"
+                    : "Cantilever chair"
+                }
                 img="https://i.pinimg.com/originals/82/34/5d/82345d989630d9b4b8dcca81eb317c46.png"
               />
             </div>
