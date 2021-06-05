6db202471da1f991204010da1c8e23c70212982e
YonJun <jhon_08_2007@hotmail.com>
2021-06-05 12:23:05 -0500

    button outline none

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -19,12 +19,13 @@ const CustomStyles = createGlobalStyle`
 
   body {
     -webkit-tap-highlight-color: ${theme`colors.purple.500`};
-    font-family: 'Josefin Sans', sans-serif;
-    ${tw`antialiased`}    
-    
-    
-
+    font-family: 'Josefin Sans', sans-serif;    
+    ${tw`antialiased`}
    }
+
+  button{    
+    ${tw`focus:outline-none`}    
+  }
 
   .btn{
     ${tw`px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md dark:bg-gray-800 hover:bg-blue-500 dark:hover:bg-gray-700 focus:outline-none focus:bg-blue-500 dark:focus:bg-gray-700`}
