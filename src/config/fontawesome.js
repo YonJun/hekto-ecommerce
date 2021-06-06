f2c29fd24fd340ff56c3a5321aacdada1a517bd6
YonJun <jhon_08_2007@hotmail.com>
2021-06-06 18:10:31 -0500

    Leatest Blog add card

--- src/config/fontawesome.js
+++ src/config/fontawesome.js
@@ -4,8 +4,22 @@ import {
   faShoppingCart,
   faSearchPlus,
   faCheck,
+  faPenFancy,
 } from "@fortawesome/free-solid-svg-icons";
-import { faHeart, faUser } from "@fortawesome/free-regular-svg-icons";
+import {
+  faHeart,
+  faUser,
+  faCalendarAlt,
+} from "@fortawesome/free-regular-svg-icons";
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 
-library.add(faCheck, faShoppingCart, faUser, faHeart, faSearchPlus, faFacebook);
+library.add(
+  faPenFancy,
+  faCalendarAlt,
+  faCheck,
+  faShoppingCart,
+  faUser,
+  faHeart,
+  faSearchPlus,
+  faFacebook,
+);
--- src/layouts/HomePage/LeatestBlog.jsx
+++ src/layouts/HomePage/LeatestBlog.jsx
@@ -1,3 +1,4 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import tw from "twin.macro";
 import { Subtitle } from "../../components/HomePage/Typography";
 
@@ -8,9 +9,35 @@ const Card = () => {
       style={{ maxWidth: 350 }}>
       <img
         src="https://sodimac.decolovers.cl/blog/wp-content/uploads/2020/08/900-seccional-nube_3430987.jpg"
-        tw="w-full h-auto block"
+        tw="w-full h-auto block rounded-xl"
       />
-      <div tw="py-3 px-2"></div>
+      <div tw="p-5 text-navy-blue">
+        <div tw="pb-5">
+          <p tw="inline-block">
+            <FontAwesomeIcon tw="text-my-pink" icon={["fas", "pen-fancy"]} />
+            <span tw="pl-2 font-medium">Saber Ali</span>
+          </p>
+          <p tw="ml-10 inline-block">
+            <FontAwesomeIcon
+              tw="text-yellow-400"
+              icon={["far", "calendar-alt"]}
+            />
+            <span tw="pl-2 font-medium">21 August,2020</span>
+          </p>
+        </div>
+        <div tw="space-y-3">
+          <h6 tw="font-bold text-lg">Top esssential Trends in 2021</h6>
+          <p tw="text-gray-500">
+            More off this less hello samlande lied much over tightly circa horse
+            taped mightly
+          </p>
+          <p>
+            <a href="#" tw="underline">
+              Read More
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
